0cfce5623149de907539eb56d3133d2aebeb9e2d
rafael52468 <rafaellima52468@gmail.com>
2022-03-24 19:07:03 -0300

    main

--- gplays-backend/gplaysapi/views.py
+++ gplays-backend/gplaysapi/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
+
+
+
+def home(request):
+    data = {}
+    return render(request, 'home.html', data)
 # Create your views here.
